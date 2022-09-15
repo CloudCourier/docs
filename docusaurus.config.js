f535c7a291638170d362602d0b90bebcb82b588a
zbl <59828992+zbl666@users.noreply.github.com>
2022-09-15 22:14:16 +0800

    chore: 切换logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/logo.svg',
+  favicon: 'img/logo.png',
   organizationName: 'CloudCourier', // Usually your GitHub org/user name.
   projectName: 'CloudCourier', // Usually your repo name.
 
